a022a47f9b65a1d66c2e3c07b92a46c1c5ac59fa
Vineet Nair <nair.vin@husky.neu.edu>
2016-12-16 23:21:52 -0500

    Final Changes

--- project/services/user.service.server.js
+++ project/services/user.service.server.js
@@ -1,101 +1,13 @@
-//    var passport = require('passport');
-//    var LocalStrategy = require('passport-local').Strategy;
-//    var bcrypt = require('bcrypt-nodejs');
-
 module.exports = function (app, models, userModel) {
 
-//    var userModel = models.userModel;
     var auth = authorized;
     app.post("/api/user", auth,createUser);
     app.get("/api/user/:userId", findUserById);
-//    app.post("/proj/login",passport.authenticate('local'), login);
-//    app.post('/proj/logout', logout);
-//    app.post('/proj/register', register);
-//    app.get('/proj/isLoggedIn', isLoggedIn);
     app.get("/api/user", findUser);
     app.put("/api/user/:userId", updateUser);
     app.delete("/api/user/:userId", auth,deleteUser);
 
 
-//    passport.use(new LocalStrategy(localStrategy));
-//    passport.serializeUser(serializeUser);
-//    passport.deserializeUser(deserializeUser);
-
- /*   function localStrategy(username, password, done) {
-        userModel
-            .findUserByUsername(username)
-            .then(
-                function (user) {
-
-                    if(user && bcrypt.compareSync(password, user.password)) {
-                        done(null, user);
-                    } else {
-                        done(null, false);
-                    }
-                },
-                function(err) {
-                    done(err);
-                }
-            );
-    }
-
-    function serializeUser(user, done) {
-        done(null, user);
-    }
-
-    function deserializeUser(user, done) {
-        userModel
-            .findUserById(user._id)
-            .then(
-                function(user){
-                    done(null, user);
-                },
-                function(err){
-                    done(err, null);
-                }
-            );
-    }
-
-    function login(req, res) {
-        var user = req.user;
-        res.json(user);
-    }
-
-    function logout(req, res) {
-        req.logOut();
-        res.status(200).send();
-    }
-
-    function register(req, res) {
-        var newUser = req.body;
-        newUser.password = bcrypt.hashSync(newUser.password);
-        userModel
-            .createUser(newUser)
-            .then(function (user) {
-                req.login(user, function (err) {
-                    if(!err){
-                        console.log(user);
-                        res.json(user);
-                    }
-                    else {
-                        res.status(400).send(err);
-                    }
-                })
-
-            })
-            .catch(function (error) {
-                res.status(400).send();
-            });
-    }
-
-    function isLoggedIn(req, res) {
-        if(req.isAuthenticated()) {
-            res.json(req.user);
-        } else {
-            res.send('0');
-        }
-    }
-*/
     function createUser(req, res) {
         var newUser = req.body;
         userModel
